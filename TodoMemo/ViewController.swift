6ada6795db5c3f8681ab161ca72d22de0b6276db
Shotaro Furuta <zoxx31syg1y999@gmail.com>
2020-09-22 02:31:09 +0900

    Initial Commit

---
+++ TodoMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoMemo
+//
+//  Created by 古田翔太郎 on 2020/09/22.
+//  Copyright © 2020 古田翔太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
