b337aab4ade8e00cd2a40c73da902b2740e7dfd1
Shotaro Furuta <zoxx31syg1y999@gmail.com>
2020-09-23 18:53:39 +0900

    ・ユーザデフォルトへの保存可能 ・横スライドでの削除可能 ・画面遷移作から戻ってくる際に，データを更新できていない． 　→アプリを一旦落とすことで，保存されていることを確認可能．

---
+++ TodoMemo/MemoViewController.swift
@@ -0,0 +1,79 @@
+//
+//  MemoViewController.swift
+//  TodoMemo
+//
+//  Created by 古田翔太郎 on 2020/09/22.
+//  Copyright © 2020 古田翔太郎. All rights reserved.
+//
+
+import UIKit
+
+//TodoListを入れるための配列
+var  todoListArray = [String]()
+
+//DateListを入れるための配列
+var  dateListArray = [String]()
+
+//UserDefaultへアクセス
+var saveData: UserDefaults = UserDefaults.standard
+
+class MemoViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var memoTextField: UITextField!
+    @IBOutlet var picker: UIDatePicker!
+    @IBOutlet var label: UILabel! //仮置き
+    
+    //var memoList:[String] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //delegateの所在がMemoViewController
+        memoTextField.delegate = self
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func saveButton() {
+        //日付のフォーマット&取得
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd"
+        label.text = "\(formatter.string(from: picker.date))"
+        
+    
+        //配列に要素(入力内容)を追加する
+        todoListArray.append(memoTextField.text!)
+        //dateListArray.append(label.text!)
+         //追加ボタンを押したらフィールドを空にする
+         memoTextField.text = ""
+        //配列の中身をUserDefaultに追加する
+        saveData.set(todoListArray, forKey: "TodoList" )
+        //saveData.set(dateListArray, forKey: "DateList" )
+        
+        //let viewController: ViewController
+        //viewController.viewDidLoad()
+        /*
+        //UserDefaultsへ書き込み&初期化
+        saveData.set(memoTextField.text, forKey: "TodoList" )
+        memoTextField.text = ""
+         */
+    }
+    
+    //Delegate：改行時にキーボードが閉じる．
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func cancelButton() {
+        // self.navigationController?.popViewController(animated: true) ←What?
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
--- TodoMemo/ViewController.swift
+++ TodoMemo/ViewController.swift
@@ -8,13 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController:  UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet var table: UITableView!
+    /*
+    //TodoListを入れるための配列
+    var  todoListArray = [String]()
+
+    //DateListを入れるための配列
+    var  dateListArray = [String]()
+
+    //UserDefaultへアクセス
+    var saveData: UserDefaults = UserDefaults.standard
+    */
+    //表示するcell数を設定
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //戻り値の設定(表示するcell数)
+        return todoListArray.count
+    }
+    
+    //表示するcellの中身を決める
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+            let todoCell = tableView.dequeueReusableCell(withIdentifier: "TodoCell")
+            
+            todoCell?.textLabel?.text = todoListArray[indexPath.row]
+            
+            return todoCell!
+        
+        //配列を作る
+        /*let TodoCell : UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "TodoCell", for: indexPath)
+        //配列の中身を作る
+        TodoCell.textLabel!.text = todoListArray[indexPath.row]
+        //戻り値の設定（表示する中身)
+        return TodoCell*/
+    }
+    
+    //Delete Button
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            todoListArray.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.delegate   = self
+        table.dataSource = self
+        
+        //追加画面で入力した内容を取得する
+        if saveData.object(forKey: "TodoList") != nil {
+            todoListArray = saveData.object(forKey: "TodoList") as! [String]
+        }
     }
 
-
+    //MemoViewControllerからViewControllerへ画面遷移するためのクラス
+    @IBAction func unwindToViewController(segue: UIStoryboardSegue) {}
 }
 
